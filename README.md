400c4d3619ebf64dd00f625a7034d81bc5b8d710
Dilan Madhusanka <kadm0128@gmail.com>
2023-12-20 22:28:17 +0800

    CHG | CSS styles and screens added | COMPLETED

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# poc-microfrontend-webpack-html
-Microfrontend POC with webpack and HTML.
+# Micro Frontend with HTML and WebPack
+
+Simple micro frontend POC application with HTML and CSS with WebPack.
+
+## Setup Instructions
+1. Run ``` npm install ```
+2. Run ``` npm run dev ```
+
+## Screens
+![alt text](screens/screen-01.PNG)
+
+![alt text](screens/screen-02.PNG)
+
+![alt text](screens/screen-03.PNG)
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 document.body.innerHTML = `
-   <h1>Micro Frontend Host</h1>
-   <div id="module1Container"></div>
-   <div id="module2Container"></div>
+   <div class="container">
+      <h1 class="header">Micro Frontend Host</h1>
+      <div class="content">
+         <div id="module1Container" class="module-container"></div>
+         <div id="module2Container" class="module-container"></div>
+         <div id="module3Container" class="d-none"></div>
+      </div>
+   </div>
 `;
 
 import('./moduleLoader');
